6a768457e27fd981d2eb57bc13eeb10d353ea179
isra diaz <israelmyname@gmail.com>
2021-05-01 22:55:26 -0500

    :sparkles primer commit con el chat

---
+++ src/components/join/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(()=> ({
+  root:{
+    backgroundColor:'red',
+    padding:'10px',
+    width:'450px',
+    height:'400px',
+  }
+}));
